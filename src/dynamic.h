2f56c5e63722768afd71cf0497eabb7f81b751de
Fredrik Widlund <fredrik.widlund@gmail.com>
2015-12-18 08:52:50 +0000

    Squashed 'lib/libdynamic/' content from commit 07237d8
    git-subtree-dir: lib/libdynamic
    git-subtree-split: 07237d85b83bef0ff96377c3e7d851e70db23540

---
+++ src/dynamic.h
@@ -0,0 +1,24 @@
+#ifndef DYNAMIC_H_INCLUDED
+#define DYNAMIC_H_INCLUDED
+
+#define DYNAMIC_VERSION "0.5.0"
+#define DYNAMIC_VERSION_MAJOR 0
+#define DYNAMIC_VERSION_MINOR 5
+#define DYNAMIC_VERSION_PATCH 0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <dynamic/buffer.h>
+#include <dynamic/vector.h>
+#include <dynamic/string.h>
+#include <dynamic/cfarmhash.h>
+#include <dynamic/mapi.h>
+#include <dynamic/maps.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DYNAMIC_H_INCLUDED */
