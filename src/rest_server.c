5b6c6933ae1fa0bfaf1d3e4862f9e226d3521352
Fredrik Widlund <fredrik.widlund@gmail.com>
2017-03-26 20:09:39 +0000

    Update

--- src/rest_server.c
+++ src/rest_server.c
@@ -14,6 +14,7 @@
 #include <sys/resource.h>
 #include <sys/param.h>
 #include <sys/wait.h>
+#include <sys/queue.h>
 #include <linux/sched.h>
 #include <netdb.h>
 #include <err.h>
@@ -22,84 +23,25 @@
 #include <clo.h>
 #include <reactor.h>
 
-static int realtime_scheduler(void)
+#include "setup.h"
+
+void plaintext(void *state, int type, void *data)
 {
-  struct sched_param param;
-  struct rlimit rlim;
-  int e;
-
-  e = sched_getparam(0, &param);
-  if (e == -1)
-    return -1;
-
-  param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-  rlim = (struct rlimit) {.rlim_cur = param.sched_priority, .rlim_max = param.sched_priority};
-  e = prlimit(0, RLIMIT_RTPRIO, &rlim, NULL);
-  if (e == -1)
-    return -1;
-
-  e = sched_setscheduler(0, SCHED_FIFO, &param);
-  if (e == -1)
-    return -1;
-
-  return 0;
-}
-
-static void plaintext(void *state, reactor_rest_request *request)
-{
-  (void) state;
-  reactor_rest_respond_text(request, "Hello, World!");
-}
-
-static void json(void *state, reactor_rest_request *request)
-{
-  char buffer[4096];
-  int e;
+  reactor_http_server_context *context = data;
 
   (void) state;
-  e = clo_encode((clo[]) {clo_object({"message", clo_string("Hello, World!")})}, buffer, sizeof(buffer));
-  if (e == 0)
-    reactor_rest_respond_body(request, 200, "OK", "application/json", buffer, strlen(buffer));
-  else
-    reactor_rest_respond_empty(request, 500, "Internal Server Error");
-}
-
-void server(void)
-{
-  reactor_rest rest;
-  int e;
-
-  (void) realtime_scheduler();
-  reactor_core_open();
-  reactor_rest_init(&rest, NULL, NULL);
-  reactor_rest_name(&rest, "*");
-  reactor_rest_open(&rest, NULL, "8080", 0);
-  reactor_rest_add_match(&rest, "GET", "/plaintext", plaintext, NULL);
-  reactor_rest_add_match(&rest, "GET", "/json", json, NULL);
-  e = reactor_core_run();
-  if (e == -1)
-    err(1, "reactor_core_run");
-  reactor_core_close();
+  (void) type;
+  reactor_http_server_respond_text(context->session, "Hello, World!");
 }
 
 int main()
 {
-  long i, n;
-  pid_t cpid;
+  reactor_http_server server;
 
-  (void) realtime_scheduler();
-  signal(SIGPIPE, SIG_IGN);
-  n = MAX(1, sysconf(_SC_NPROCESSORS_ONLN) / 2);
-  for (i = 0; i < n; i ++)
-    {
-      cpid = fork();
-      if (cpid == -1)
-        err(1, "fork");
-      if (cpid == 0)
-        {
-          server();
-          exit(0);
-        }
-    }
-  wait(NULL);
+  setup(2, 0);
+  reactor_core_construct();
+  reactor_http_server_open(&server, NULL, &server, "0.0.0.0", "8080");
+  reactor_http_server_route(&server, plaintext, NULL, "GET", "/plaintext");
+  reactor_core_run();
+  reactor_core_destruct();
 }
